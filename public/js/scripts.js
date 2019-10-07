8d4ab1ca33e630051d85a40d80bb7e93d89f01a5
ehsan azimi <azimi.ehsan66@gmail.com>
2019-10-07 17:58:16 +0330

    63216351

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,6 +1,78 @@
+/*
 //console.log($);
 
 $('#letter-selection').on('change',function(e){
     //console.log($(this).val());
-    
-});
+   $.ajax({
+       url:"/actors/"+$(this).val(),
+       success : function (res) {
+      //     console.log(res);
+
+        //   $('.actor-list').html("<li>ali kalant</li>");
+
+
+let html = "";
+
+           $.each(res,function (i,v) {
+
+              html += "<li>";
+               html += "<a href='#'>";
+               html += v.first_name+" "+v.last_name;
+               html += "</a>";
+               html += "</li>";
+
+               $('.actor-list').html(html);
+           });
+
+       }
+   });
+}); */
+
+
+
+(function ($,H) {
+
+    var Actor ={
+
+        init :function (config) {
+
+            this.c = config;
+            this.bindEvents();
+        },
+
+        bindEvents : function () {
+          this.c.letterSelection.on('change',this.fetchActorsBylastname)
+
+
+            this.c.actor.on('click',function () {
+                console.log("ehsan");
+            })
+        },
+        fetchActorsBylastname : function () {
+           $.ajax({
+               url: "/actors/" + $(this).val(),
+               success : function (res) {
+                   console.log(res);
+               }
+           })
+
+
+
+        }
+
+    };
+
+    Actor.init({letterSelection : $('#letter-selection')
+    ,   actor : $('.actor-list')
+    });
+
+
+})(jQuery,handlebars)
+
+
+
+
+
+
+
+
