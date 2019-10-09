9a965580b9d9164113f7b5420e14f3409a4a583a
ehsan azimi <azimi.ehsan66@gmail.com>
2019-10-09 12:26:09 +0330

    dsfsdf

--- 001.js
+++ 001.js
@@ -8,16 +8,34 @@ let d = fs.readFile('data','utf8',(err,d)=>{
 */
 
 
+/*
 
 
-function readFileP() {
-    return new promise((resolve,reject)=>{
-        fs.readFile("data","utf8",(err,data)=>{
+function readFileP(filename) {
+   return new Promise((resolve,reject)=>{
+        fs.readFile(filename,"utf8",(err,data)=>{
             if (err){reject(err)}
             resolve(data);
         })
 
     });
+
 }
 
-readFileP();
+console.log(readFileP("data1").then((data)=>{
+    console.log(data);
+}
+).catch((err)=>{
+        console.log(err);
+    })
+
+
+);*/
+
+
+
+
+try{console.log(JSON.parse('{"name":"ali"}'));}catch (ex) {
+    console.log(ex);
+
+}
